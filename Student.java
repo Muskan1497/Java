de6699fcd5fb3c850736ce4f5994354e4ab278b3
Muskan1497 <muskan14705@gmail.com>
2026-08-16 23:51:28 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public static void main(String[] args) {
+        // Declaring variables
+        String studentName = "Muskan";
+        int rollNumber = 9;
+        int marksSubject1 = 88;
+        int marksSubject2 = 92;
+        int marksSubject3 = 83;
+
+        // Printing the details
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks in Subject 1: " + marksSubject1);
+        System.out.println("Marks in Subject 2: " + marksSubject2);
+        System.out.println("Marks in Subject 3: " + marksSubject3);
+    }
+}
